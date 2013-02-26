da305c20af5ca959635e1889419b8dd054fcfd1f
Bayan Khalili <bayan.net@gmail.com>
2013-02-26 19:25:49 +1100

    Last word count

--- jobs/raw-mapper.rb
+++ jobs/raw-mapper.rb
@@ -1,22 +1,5 @@
-def go
-  ARGF.each do |line|
-    line = line.chomp
-    fields = line.split("\t")
-    for token in tokenize(fields[51])
-      puts "#{token}\t1" if acceptable?(token)
-    end
+ARGF.each do |line|
+  line.gsub(/["',;:]/,'').scan(/\S+[.!?]/).each do |last_word|
+    puts "#{last_word.gsub(/[.!?]/,'')}\t1"
   end
 end
-
-def tokenize string
-  string.split(/\s+/).map do |token|
-    stripped = token.gsub(/[^a-zA-Z0-9\']+/, '')
-    stripped.downcase
-  end
-end
-
-def acceptable? token
-  (3..100).include?(token.length)
-end
-
-go
